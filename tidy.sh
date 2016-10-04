b1f70d92f5c6181e83102a1aac7f0916c36841bd
Josh Peterson <petersonjm1@gmail.com>
2016-10-04 06:18:19 -0400

    Only check the changed .cpp files with clang-tidy

--- tidy.sh
+++ tidy.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 if [ $# -eq 0 ] ; then
-  FILES="src/*.cpp test/*.cpp"
+  #FILES="src/*.cpp test/*.cpp"
+  FILES=`git diff  --name-only --diff-filter=AMR '*.cpp' | xargs`
 else
   FILES="$1"
 fi
+echo Checking $FILES
 clang-tidy $FILES -config='' -- -std=c++14 -I/usr/lib/llvm-3.8/include
