a6c33853b19a2ff514da74efefa228a87adbf31d
Josh Peterson <petersonjm1@gmail.com>
2016-12-01 07:02:21 -0500

    Add a usage output.

--- exe/main.cpp
+++ exe/main.cpp
@@ -1,9 +1,11 @@
 #include "../include/driver.hpp"
 #include <algorithm>
+#include <cstring>
+#include <iostream>
 #include <string>
 #include <vector>
 
-static std::vector<std::string> transformArguments(int number, const char** argv)
+static std::vector<std::string> TransformArguments(int number, const char** argv)
 {
   std::vector<std::string> arguments;
   if (number != 0)
@@ -15,8 +17,25 @@ static std::vector<std::string> transformArguments(int number, const char** argv
   return arguments;
 }
 
+static void PrintUsage()
+{
+  std::cout << "Usage: layout <source file> [compiler arguments]\n";
+}
+
+static bool Equals(const char* standard, const char* candidate1,
+                   const char* candidate2)
+{
+  return strcmp(standard, candidate1) == 0 || strcmp(standard, candidate2) == 0;
+}
+
 int main(int argc, const char* argv[])
 {
-  // This line violates cppcoreguidelines-pro-bounds-pointer-arithmetic
-  return ComputeLayout(argv[1], transformArguments(argc - 2, &argv[2])); // NOLINT
+  // These lines violate cppcoreguidelines-pro-bounds-pointer-arithmetic
+  if (argc < 2 || (argc == 2 && Equals(argv[1], "--help", "-h"))) // NOLINT
+  {
+    PrintUsage();
+    return 1;
+  }
+
+  return ComputeLayout(argv[1], TransformArguments(argc - 2, &argv[2])); // NOLINT
 }
