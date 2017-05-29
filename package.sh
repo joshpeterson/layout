4b2c7d853abc71c3edfd363d96f25448d6c3a89b
Josh Peterson <petersonjm1@gmail.com>
2017-05-29 06:45:31 -0400

    Package all of the standard C and C++ headers

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 if [ -f layout.tar.gz ]; then
   rm layout.tar.gz
 fi
@@ -7,7 +7,24 @@ if [ -d cde-package ]; then
 fi
 ./release.sh
 cde ./layout
-tar -czvf layout.tar.gz cde-package/ layout
+
+declare -a rdirectories=("/usr/include/c++/6"
+                         "/usr/include/c++/6/backward"
+                         "/usr/include/x86_64-linux-gnu")
+for i in "${rdirectories[@]}"
+do
+  echo $i
+  cp -r --parents $i/* cde-package/cde-root
+done
+
+declare -a directories=("/usr/include"
+                        "/usr/lib/llvm-3.8/lib/clang/3.8.0/include")
+for j in "${directories[@]}"
+do
+  cp --parents $j/* cde-package/cde-root
+done
+
+tar -czf layout.tar.gz cde-package/
 if [ -d cde-package ]; then
   rm -rf cde-package/
 fi
