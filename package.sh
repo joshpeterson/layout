4fe7c12c6fd83b0e90073319717106faa5ba5f6c
Josh Peterson <petersonjm1@gmail.com>
2018-03-14 07:05:18 -0400

    Update package script to work without CDE

--- package.sh
+++ package.sh
@@ -2,32 +2,36 @@
 if [ -f layout.tar.gz ]; then
   rm layout.tar.gz
 fi
-if [ -d cde-package ]; then
-  rm -rf cde-package/
+if [ -d layout-package ]; then
+  rm -rf layout-package/
 fi
+
 ./release.sh
-cde ./build/Release/layout
+
+mkdir layout-package
+cp build/Release/layout layout-package
+cp /usr/lib/llvm-5.0/lib/libclang-5.0.so.1 layout-package
+cp /usr/lib/llvm-5.0/lib/libLLVM-5.0.so.1 layout-package
+cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 layout-package
+cp /lib/x86_64-linux-gnu/libtinfo.so.5 layout-package
 
 declare -a rdirectories=("/usr/include/c++/7.2.0"
                          "/usr/include/c++/7.2.0/backward"
                          "/usr/include/x86_64-linux-gnu")
 for i in "${rdirectories[@]}"
 do
-  echo $i
-  cp -r --parents $i/* cde-package/cde-root
+  cp -r --parents $i/* layout-package
 done
 
 declare -a directories=("/usr/include"
                         "/usr/lib/llvm-5.0/lib/clang/5.0.0/include")
 for j in "${directories[@]}"
 do
-  cp --parents $j/* cde-package/cde-root
+  # Don't print messages about omitting a directory
+  cp --parents $j/* layout-package 2>&1 | grep -v "omitting directory"
 done
 
-tar -czf layout.tar.gz cde-package/
-if [ -d cde-package ]; then
-  rm -rf cde-package/
-fi
-if [ -f cde.options ]; then
-  rm cde.options
+tar -czf layout.tar.gz layout-package/
+if [ -d layout-package ]; then
+  rm -rf layout-package/
 fi
